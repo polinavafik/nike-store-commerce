fcea57f9c1f819c273083c2067b7e844db132d84
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-11-20 16:50:51 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Hero } from "./components";
+import { heroapi } from "./data/data";
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <Hero heroapi={heroapi} />
+      </main>
+    </>
+  );
+};
+
+export default App;
